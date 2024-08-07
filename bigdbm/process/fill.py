b421582ea4acb1f096ad2b51630da3f7d78f2d21
Prerit Das <preritdas@gmail.com>
2024-08-07 21:28:56 +0000

    Logging for FillProcessor.

--- bigdbm/process/fill.py
+++ bigdbm/process/fill.py
@@ -41,16 +41,24 @@ class FillProcessor(BaseProcessor):
 
         while len(return_md5s) < n_hems:  # Constantly pull PII until the threshold is hit
             if not md5s_bank:
+                self.client._log("debug", "No more MD5s to pull.")
                 break
 
             n_delta: int = n_hems - len(return_md5s)
+            self.client._log("debug", f"Pulling {n_delta} more MD5s.")
+
             md5s_job: list[UniqueMD5] = md5s_bank[:n_delta]
             md5s_with_pii: list[MD5WithPII] = self.client.pii_for_unique_md5s(md5s_job)
 
             # Utilize parameterized validators to filter leads
             validator: BaseValidator
             for validator in validators:
+                initial_len: int = len(md5s_with_pii)
                 md5s_with_pii = validator.validate(md5s_with_pii)
+                self.client._log(
+                    "debug", 
+                    f"{validator.__class__.__name__} removed {initial_len - len(md5s_with_pii)} leads."
+                )
 
             # Add post-validated (remaining) leads
             return_md5s.extend(md5s_with_pii)
@@ -73,6 +81,8 @@ class FillProcessor(BaseProcessor):
         If the initial pull does not return enough data, the processor will try again without
         the fallback validators, retaining whatever leads were already pulled.
         """
+        self.client._log("debug", f"Using FillProcessor to process job: {iab_job}")
+
         # Extend the number of hems in the initial job
         n_hems: int = iab_job.n_hems  # true amount of PII to return
         iab_job.n_hems = int(iab_job.n_hems * self.intent_multiplier)
@@ -86,9 +96,16 @@ class FillProcessor(BaseProcessor):
 
         # If we have enough leads, return them
         if len(return_md5s) >= n_hems:
+            self.client._log("debug", "Enough leads found with all validators.")
             return return_md5s
 
         # If we don't have enough leads, try again with fallen back validators
+        self.client._log(
+            "debug", 
+            f"Only {len(return_md5s)} leads found with all validators. Retrying without: {
+                [v.__class__.__name__ for v in self.fallback_validators]
+            }"
+        )
         existing_md5s: list[str] = [i.md5 for i in return_md5s]
         return_md5s += self._pull_and_validate(
             [i for i in intent_events if i.md5 not in existing_md5s],  # only run on events not used
@@ -96,6 +113,7 @@ class FillProcessor(BaseProcessor):
             self.required_validators  # only required, no fallback validators
         )
 
+        self.client._log("debug", f"Returning {len(return_md5s)} leads after removing fallback validators.")
         return return_md5s
 
 
