3c675363eab2b45f3cc267060b3dbce7b0a0f530
Prerit Das <preritdas@gmail.com>
2024-08-20 03:38:37 +0000

    Validation inclusive insights in a new class.

--- bigdbm/analyze/insights/__init__.py
+++ bigdbm/analyze/insights/__init__.py
@@ -39,6 +39,65 @@ class LeadInsights(BaseModel):
 class OpenAIInsightsGenerator(BaseAnalyzer):
     """Generates insights from PII data using OpenAI."""
 
+    def __init__(self, openai_api_key: str):
+        try:
+            from openai import OpenAI
+        except ImportError:
+            raise ImportError("Please install this package with the 'openai' extra.")
+        
+        self.openai_client: OpenAI = OpenAI(api_key=openai_api_key)
+
+    def analyze(self, pii_md5s: list[MD5WithPII]) -> str:
+        """
+        Analyze the list of MD5s with PII and generate insights using an LLM.
+
+        Args:
+            pii_md5s (list[MD5WithPII]): List of MD5 hashes with associated PII data.
+
+        Returns:
+            str: Generated insights as a string.
+        """
+        result = self.openai_client.beta.chat.completions.parse(
+            model="gpt-4o-2024-08-06",
+            messages=[
+                {
+                    "role": "system",
+                    "content": SYSTEM_PROMPT
+                },
+                {
+                    "role": "user",
+                    "content": CSVStringFormatter().format_md5s(pii_md5s)
+                }
+            ],
+            max_tokens=4095,
+            temperature=1,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            response_format=LeadInsights
+        )
+
+        lead_insights: LeadInsights | None = result.choices[0].message.parsed
+
+        if not lead_insights:
+            return "No insights on these leads at the moment."
+
+        # Process insights to create an ordered list
+        processed_insights: list[str] = []
+        for i, insight in enumerate(lead_insights.insights, start=1):
+            # Remove "- " if present at the beginning of the insight
+            if insight.startswith("- "):
+                insight = insight[2:]
+
+            # Add the ordered list number
+            processed_insights.append(f"{i}. {insight}")
+
+        return "\n".join(processed_insights)
+
+
+class ValidationInclusiveInsightsGenerator(BaseAnalyzer):
+    """Generates insights from PII data using OpenAI. Incorporates knowledge of validators."""
+
     def __init__(self, openai_api_key: str, validators: list[BaseValidator] = []):
         try:
             from openai import OpenAI
