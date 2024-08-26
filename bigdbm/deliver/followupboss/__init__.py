2dda45dd7cb55706de7bcf9cb6268fbeab6f4853
Prerit Das <preritdas@gmail.com>
2024-08-26 06:59:33 +0000

    Expose the AI deliverer as well.
    [skip ci]

--- bigdbm/deliver/followupboss/__init__.py
+++ bigdbm/deliver/followupboss/__init__.py
@@ -1,2 +1,3 @@
 """Deliverers to interact with Follow Up Boss."""
 from bigdbm.deliver.followupboss.vanilla import FollowUpBossDeliverer
+from bigdbm.deliver.followupboss.ai_fields import AIFollowUpBossDeliverer
