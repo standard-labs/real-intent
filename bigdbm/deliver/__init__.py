99630a06717da666e94c8b6b0775e89307f7fcd3
Prerit Das <preritdas@gmail.com>
2024-08-28 22:48:39 +0000

    Import KvCoreDeliverer in deliver/__init__.py

--- bigdbm/deliver/__init__.py
+++ bigdbm/deliver/__init__.py
@@ -1,2 +1,3 @@
 """Take a list of MD5WithPII (leads) and format/deliver them. Do something with them, essentially."""
 from bigdbm.deliver.csv import CSVStringFormatter
+from bigdbm.deliver.kvcore import KvCoreDeliverer
