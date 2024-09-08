461b7fcd1f1745dfe187367c2e3d2ab8cf0da76c
Prerit Das <preritdas@gmail.com>
2024-09-08 05:30:14 +0000

    First pass at kvCORE deliverer.

---
+++ real_intent/deliver/kvcore/__init__.py
@@ -0,0 +1,100 @@
+"""Deliver to kvCORE."""
+import requests
+
+from real_intent.internal_logging import log
+from real_intent.deliver.base import BaseOutputDeliverer
+from real_intent.schemas import MD5WithPII
+
+
+EMAIL_TEMPLATE: str = """First Name: {first_name}
+Last Name: {last_name}
+Email: {email}"""
+
+
+class KVCoreDeliverer(BaseOutputDeliverer):
+    """
+    Deliver to kvCORE. Does not use the API - uses the inboxing address.
+
+    The inboxing address is the email address that kvCORE uses to import leads.
+    The tag is the hashtag that kvCORE uses to tag leads.
+    """
+
+    def __init__(self, postmark_server_token: str, from_email: str, inboxing_address: str, tag: str):
+        """Initialize the deliverer."""
+        # Postmark setup
+        self.postmark_server_token = postmark_server_token
+        self.from_email = from_email
+
+        # kvCORE setup
+        self.inboxing_address = inboxing_address
+        self.tag = tag
+
+    def _deliver(self, pii_md5s: list[MD5WithPII]) -> bool:
+        """
+        Deliver the leads to kvCORE.
+
+        Returns True if all leads were delivered successfully.
+        Otherwise, returns False.
+        """
+        for pii_md5 in pii_md5s:
+            if not self._deliver_one(pii_md5):
+                return False
+
+        return True
+
+    def _deliver_one(self, pii_md5: MD5WithPII) -> bool:
+        """
+        Email one lead into the inboxing address.
+
+        Returns True if the lead was delivered successfully.
+        Otherwise, returns False.
+        """
+        response = requests.post(
+            "https://api.postmarkapp.com/email",
+            headers={
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "X-Postmark-Server-Token": self.postmark_server_token
+            },
+            json={
+                "From": self.from_email,
+                "To": self.inboxing_address,
+                "Subject": "Add Contact",
+                "TextBody": self._email_body(pii_md5)
+            }
+        )
+
+        if not response.ok:
+            log.error(f"Failed to send email to kvCORE: {response.text}")
+            return False
+
+        return True
+
+    def _email_body(self, pii_md5: MD5WithPII) -> str:
+        """Create the email body."""
+        assert pii_md5.pii.first_name, "First name is required."
+        assert pii_md5.pii.last_name, "Last name is required."
+        assert pii_md5.pii.emails, "Email is required."
+
+        email_body: str = EMAIL_TEMPLATE.format(
+            first_name=pii_md5.pii.first_name,
+            last_name=pii_md5.pii.last_name,
+            email=pii_md5.pii.emails[0],
+        )
+
+        # Add phone number if available
+        if pii_md5.pii.phone_numbers:
+            if pii_md5.pii.mobile_phones[0].do_not_call:
+                log.warn("Importing a DNC phone number into kvCORE.")
+
+            email_body += f"\nPhone: {pii_md5.pii.mobile_phones[0].phone}"
+
+        # Add zip code if available
+        if pii_md5.pii.zip_code:
+            email_body += f"\nZipcode: {pii_md5.pii.zip_code}"
+
+        # Add tag if specified
+        if self.tag:
+            email_body += f"\nHashtag: {self.tag}"
+
+        return email_body
