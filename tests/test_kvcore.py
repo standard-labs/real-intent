cfb784da9f3e8f4561ba9ecee598f528203821c5
Prerit Das <preritdas@gmail.com>
2024-09-22 00:23:17 +0000

    Basic email body test for kvCORE integration.

---
+++ tests/test_kvcore.py
@@ -0,0 +1,18 @@
+"""Basic kvCORE tests, without actually integrating."""
+from real_intent.deliver.kvcore import KVCoreDeliverer
+
+
+def test_kvcore_email_body(sample_pii_md5s) -> None:
+    """
+    Test generating the email body.
+
+    We can assume the following values are in the email body _because_ they are
+    in the sample PIIs.
+    """
+    deliverer = KVCoreDeliverer("", "", "", "")
+    email_body: str = deliverer._email_body(sample_pii_md5s[0])
+
+    assert "First Name" in email_body, "First Name should be in email body"
+    assert "Last Name" in email_body, "Last Name should be in email body"
+    assert "Email" in email_body, "Email should be in email body"
+    assert "Phone" in email_body, "Phone should be in email body"
