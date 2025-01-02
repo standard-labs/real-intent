8972e9ed900a3d0258368e46f512381dff1128ba
Ayush Shah <111246584+ayush-shah12@users.noreply.github.com>
2025-01-02 01:54:54 +0000

    zapier deliverer class updated

--- real_intent/deliver/zapier/__init__.py
+++ real_intent/deliver/zapier/__init__.py
@@ -1,67 +1,94 @@
-"""Deliver to Zapier webhook"""
+"""Deliver to Zapier webhooks."""
 import requests
 from typing import Any
+from functools import partial
 
 from real_intent.internal_logging import log
 from real_intent.deliver.base import BaseOutputDeliverer
 from real_intent.schemas import MD5WithPII
+from datetime import datetime
 
+from concurrent.futures import ThreadPoolExecutor
 
 class ZapierDeliverer(BaseOutputDeliverer):
     """Deliver to Zapier webhook"""
 
-    def __init__(self, webhook_url: str, client_email, per_lead_insights: dict[str, str] = {}):
+    def __init__(self, webhook_urls: list[str], per_lead_insights: dict[str, str] = {}):
         """Initialize the deliverer"""
-
-        self.webhook_url: str = webhook_url
-        self.client_email: str = client_email
+        self.webhook_urls: str = webhook_urls
         self.per_lead_insights: dict[str, str] = per_lead_insights
 
-    def _deliver(self, pii_md5s: list[MD5WithPII]) -> bool:
-        """Deliver the formatted data to Zapier webhook."""
 
+    def _warn_dnc(self, pii_md5s: list[MD5WithPII]) -> None:
+        """Log a warning if any of the leads are on the DNC list."""
+        for md5_with_pii in pii_md5s:
+            if any(phone.do_not_call for phone in md5_with_pii.pii.mobile_phones):
+                log(
+                    "warn",
+                    (
+                        f"At least 1 lead in the Follow Up Boss deliverer was on "
+                        f"the DNC list. Please validate the lead before delivery."
+                    )
+                )
+                break
+
+
+    def _deliver(self, pii_md5s: list[MD5WithPII]) -> bool:
+        """Deliver the formatted data to Zapier webhooks."""
+
+        self._warn_dnc(pii_md5s)
         payload = self._format(pii_md5s)
-        response = requests.post(self.webhook_url, json=payload)
+
+        # payload will be constant, so we can use partial to make it as a prefilled argument
+        with ThreadPoolExecutor(max_workers=5) as executor:
+            deliver_one = partial(self._deliver_one_url, payload)
+            results = list(executor.map(deliver_one, self.webhook_urls))
+        return all(results)
+
+
+    def _deliver_one_url(self, payload: list[dict[str, Any]], webhook_url: str) -> bool:
+        """Deliver batch of leads to a singular Zapier webhook."""
         
-        if not response.ok:
-            log.error(f"Failed to deliver to Zapier webhook {self.webhook_url}: {response.status_code}, {response.text}")
-            return False
-        
-        return response.ok
+        response = requests.post(webhook_url, json=payload)
+
+        if response.status_code != 200:
+            log(
+                "error",
+                (
+                    f"Failed to deliver to Zapier webhook {webhook_url}. "
+                    f"Status code: {response.status_code}, "
+                    f"Response: {response.text}"
+                )
+            )
+            
+        return response
+    
     
     def _format(self, pii_md5s: list[MD5WithPII]) -> list[dict[str, Any]]:
         """
         Format the leads into a deliverable format.
 
-        Seperates phone numbers and emails (convert_dict_lead_export()), adds insights to the dict, and seperates 
-        unique sentences.
+        Seperates phone numbers and emails (convert_dict_lead_export()), adds insights to the dict, and combines 
+        sentences into a single string.
         
         Returns the formatted list of these dictionary objects:
         [
             {
-                "md5": "123",
+                "md5": "...",
                 "pii": {...},
-                "insight": "insight for md5 123",
-                "sentence_1": "...",
-                "sentence_2": "...",
-                "sentence_3": "...",
-                "sentence_4": "...",
-                ...
+                "insight": "...",
+                "sentences": "... | ... | ... | ...",
+                "date_delivered": "...."
             },
             {
-                "md5": "456",
+                "md5": "...",
                 "pii": {...},
                 "insight": "",
-                "sentence_1": "...",
-                "sentence_2": "...",
-                "sentence_3": "...",
+                "sentences": "... | ... | ...",
+                "date_delivered": "...."
                 ...
             },
         ]
-
-        Note: Sentence seperation is done here, but might not be necessary if we don't need to send
-        sentences. Only did it here because it is able to be sent easily for the WiseAgent webhook.
-        Not sure if sentences are meant to be seperated or not...
         """
 
         formatted_leads = []
@@ -69,14 +96,16 @@ class ZapierDeliverer(BaseOutputDeliverer):
 
             md5_dict = pii_md5.convert_dict_lead_export()
 
+            # convert all values to string, needed for Zapier consistency
+            md5_dict["pii"] = {key: (str(value) if value is not None else None) for key, value in md5_dict["pii"].items()}
+
             md5_dict["insight"] = self.per_lead_insights.get(md5_dict["md5"], "")
 
-            for pos, sentence in enumerate(md5_dict["sentences"], start=1):
-                md5_dict[f"sentence_{pos}"] = sentence
-            del md5_dict["sentences"]
+            md5_dict["sentences"] = " | ".join(md5_dict["sentences"])
 
-            md5_dict["client_email"] = self.client_email
+            md5_dict["date_delivered"] = datetime.now().strftime("%Y-%m-%d")
 
             formatted_leads.append(md5_dict)
 
         return formatted_leads
+
--- tests/test_zapier.py
+++ tests/test_zapier.py
@@ -2,46 +2,42 @@ from real_intent.deliver.zapier import ZapierDeliverer
 from real_intent.schemas import PII, MD5WithPII
 
 def test_zapier_deliverer():
-    """ This sends it to the Wise Agent Integration Only...
 
-    Couple of things it tests:
-    - tests the case where no insights are provided
-    - tests the case where no sentences are provided
-
-    Everything else is a required field in the PII schema
-    
-    """
-
-    pii_1 = PII.create_fake(seed=88)
+    """ Test to mimick how the deliverer will work in real-intent-deliveries"""
+    pii_1 = PII.create_fake(seed=55)
     test1 = MD5WithPII(
             md5="123",
             sentences=["test sentence for first md5 123", "another test sentence for first md5 123", "third test sentence for first md5 123", "fourth test sentence for first md5 123"],
             pii=pii_1
         )
     
-    pii_2 = PII.create_fake(seed=21)
+    pii_2 = PII.create_fake(seed=66)
     test2 = MD5WithPII(
             md5="456",
             sentences=["sentence for second md5 456", "sentence for second md5 456", "another sentence for second md5 456"],
             pii=pii_2
         )
     
-    pii_3 = PII.create_fake(seed=25)
+    pii_3 = PII.create_fake(seed=77)
     test3 = MD5WithPII(
             md5="789",
-            sentences=[],
+            sentences=["sentence for third md5 789"],
             pii=pii_3
         )
-    
-    md5_list = [test1, test2, test3]
-    
+        
     insights = {
         "123": "Insight for first md5 123",
         "789": "Insight for third md5 789",
+        "456": "Insight for second md5 456"
     }
 
-    deliverer = ZapierDeliverer(webhook_url="https://hooks.zapier.com/hooks/catch/21128362/28ah4g6/", client_email="test@email.com", per_lead_insights=insights)
+    md5_list = [test1, test2, test3]
 
-    response = deliverer.deliver(md5_list)
+    # [wiseagent, excel] - full functional webhooks with zapier, will have to manually check, but they are working
+    webhook_urls = ["https://hooks.zapier.com/hooks/standard/21128362/135959053d3748f28471c65abd95fc3c/", "https://hooks.zapier.com/hooks/standard/21128362/8eacd04a01e340b88b8e3723e108bc15/"]
 
-    assert response == True
+    deliverer = ZapierDeliverer(webhook_urls=webhook_urls, per_lead_insights=insights)
+
+    deliverer.deliver(md5_list)
+
+    assert True
