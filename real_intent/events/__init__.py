fb5cc8b72e1b72bc53c5d2f21e7d9fd82d7be309
Prerit Das <preritdas@gmail.com>
2025-01-06 00:00:31 +0000

    Expose datatype import.

--- real_intent/events/__init__.py
+++ real_intent/events/__init__.py
@@ -1,4 +1,4 @@
 """Event generation."""
-from real_intent.events.models import EventsResponse
+from real_intent.events.models import Event, EventsResponse
 from real_intent.events.perplexity import PerplexityEventsGenerator
 from real_intent.events.scrapy_claude import ScrapybaraEventsGenerator
