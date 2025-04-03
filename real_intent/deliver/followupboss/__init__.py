a7d7fcc3b88d4e9e7e9e35ec591fdea0881f31b6
Ayush <ayushsh285@gmail.com>
2025-04-03 19:34:24 +0000

    fix imports

--- real_intent/deliver/followupboss/__init__.py
+++ real_intent/deliver/followupboss/__init__.py
@@ -1,3 +1,3 @@
 """Deliverers to interact with Follow Up Boss."""
-from real_intent.deliver.followupboss.vanilla import FollowUpBossDeliverer, InvalidFUBCredentialsError, AccountInactiveError
+from real_intent.deliver.followupboss.vanilla import FollowUpBossDeliverer
 from real_intent.deliver.followupboss.ai_fields import AIFollowUpBossDeliverer
